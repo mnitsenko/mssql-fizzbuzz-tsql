ab809194bfcdca68262b90ce61b339ca115dd98d
mnitsenko <42030566+mnitsenko@users.noreply.github.com>
2020-08-10 14:45:51 +0600

    Add files via upload

---
+++ FizzBuzz.sql
@@ -0,0 +1,15 @@
+﻿WITH nums AS
+   (SELECT 1 AS value
+    UNION ALL
+    SELECT value + 1 AS value
+    FROM nums
+    WHERE nums.value <= 99)
+SELECT 
+  [value], 
+  CASE 
+    WHEN (([value] % 3 = 0) AND ([value] % 5 = 0)) THEN 'fizzbuzz'
+    WHEN ([value] % 3 = 0) THEN 'fizz'
+    WHEN ([value] % 5 = 0) THEN 'buzz'    
+    ELSE '' 
+  END AS [result]
+FROM nums    
